9e0a7cc70d5d2b9f05d9de766b944d4bd8c6c936
Raphaël Ceré <raphaelcere@hotmail.com>
2025-08-26 14:22:55 +0200

    Initial structure for ed318

---
+++ monitoring/uss_qualifier/scenarios/eurocae/ed318/source_data_model.py
@@ -0,0 +1,65 @@
+import json
+from typing import List
+
+from implicitdict import ImplicitDict, StringBasedDateTime
+# from uas_standards.eurocae_ed269 import UASZoneVersion
+
+from monitoring.uss_qualifier.resources.eurocae.ed318.source_document import (
+    SourceDocument,
+)
+from monitoring.uss_qualifier.scenarios.scenario import TestScenario
+from monitoring.uss_qualifier.suites.suite import ExecutionContext
+
+
+# TODO: When the format is confirmed, this should be moved to uas_standards.eurocae_ed269
+# class ED269SchemaFile(ImplicitDict):
+#    formatVersion: str
+#    createdAt: StringBasedDateTime
+#    UASZoneList: List[UASZoneVersion]
+
+
+class SourceDataModelValidation(TestScenario):
+    source_document: SourceDocument
+
+    def __init__(self, source_document: SourceDocument):
+        super().__init__()
+        self.source_document = source_document
+
+    def run(self, context: ExecutionContext):
+        self.begin_test_scenario(context)
+
+        self.record_note(
+            "Document",
+            f"Ready at {self.source_document.specification.url}",
+        )
+
+        self.begin_test_case("ED-318 data model compliance")
+        self.begin_test_step("Valid source")
+
+        data = None
+        with self.check(
+            "Valid JSON", [self.source_document.specification.url]
+        ) as check:
+            try:
+                data = json.loads(self.source_document.raw_document)
+            except json.decoder.JSONDecodeError as e:
+                check.record_failed(
+                    summary="Unable to deserialize the document as JSON",
+                    details=str(e),
+                )
+
+        # if data:
+        #    with self.check(
+        #        "Valid schema and values", [self.source_document.specification.url]
+        #    ) as check:
+        #        try:
+        #            ImplicitDict.parse(data, ED269SchemaFile)
+        #        except ValueError as e:
+        #            check.record_failed(
+        #                summary="Invalid format error",
+        #                details=str(e),
+        #            )
+
+        self.end_test_step()
+        self.end_test_case()
+        self.end_test_scenario()
